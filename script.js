dbac6b6cac302eaa0ddf38b630ea5e5b71c60040
abir364 <abirh364@gmail.com>
2022-08-04 04:51:23 +0600

    Added the computer choice method in js file

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+function getComputerChoice(){
+    const sel = new Map();
+    sel.set(1, 'rock');
+    sel.set(2, 'paper');
+    sel.set(3, 'scissors');
+    const a = randomIntFromInterval(1,3);
+    return sel.get(a);
+}
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+function gamelogic(){
+    const playerSelection = prompt("Enter your selection").toLowerCase();
+    const computerChoice = getComputerChoice();
+    const result
+    console.log(playerSelection, computerChoice);
+}
