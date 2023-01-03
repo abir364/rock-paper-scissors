6771a76d9e384eb911ae867eeb22bd5d87f0d465
abir364 <abirh364@gmail.com>
2023-01-04 05:00:30 +0600

    Changes to internal logic of the game(work in progress)

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let player = 0;
+let computer = 0;
+let round = 0;
+
+
 //the following function is a random number generator in a specified range where the range starts from min and ends at max
 function randomIntFromInterval(min, max) { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -13,59 +18,56 @@ function getComputerChoice(){
 }
 
 function playRound(playerSelection, computerChoice){
-    const result = new Map();
-    result.set('rock paper', 'You lose! Paper beats Rock');
-    result.set('rock rock', 'Draw!');
-    result.set('rock scissors', 'You win! Rock beats Scissors');
-    result.set('paper rock', 'You win! Paper beats Rock');
-    result.set('paper paper', 'Draw!');
-    result.set('paper scissors', 'You lose! Scissors beats Paper');
-    result.set('scissors rock', 'You lose! Rock beats Scissors');
-    result.set('scissors scissors', 'Draw!');
-    result.set('scissors paper', 'You win! Scissors beats Paper');
-    let choice = playerSelection + ' ' + computerChoice;
-    return result.get(choice);
+    if(playerSelection === 'rock' && computerChoice === 'scissors' || playerSelection === 'paper' && computerChoice === 'rock' || playerSelection === 'scissors' && computerChoice === 'paper'){
+        player++;
+        console.log('player wins');
+        round++;
+    }
+    else if(playerSelection === 'rock' && computerChoice === 'paper' || playerSelection === 'scissors' && computerChoice === 'rock' || playerSelection === 'paper' && computerChoice === 'scissors'){
+        computer++;
+        console.log('computer wins');
+        round++;
+    }
+    else{
+        console.log('Draw');
+        round++;
+    }
 }
 
-function iniate(){
-    let playerSelection = prompt("Enter your selection").toLowerCase();
-    let computerChoice = getComputerChoice();
-    return playRound(playerSelection,computerChoice);
+function gameEnd(){
+    if(round === 5){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
-function game(){
-    let player = 0;
-    let computer = 0;
-    for (let i = 0; i < 5; i++) {
-        let result = iniate();
-        console.log(result);
-        if (result.includes('You win!')) {
-            player++;
-        } else if (result.includes('Draw!')){
-        }else {
-            computer++;
-        }
-        console.log('Player:'+player+'-----'+'Computer:'+computer);
+function showEndGame(){
+    if(player>computer){
+        console.log('player wins');
     }
-    if (player>computer) {
-        console.log('Player wins overall');
-    } else if (computer>player) {
-        console.log('Computer wins overall');
+    else if(player<computer){
+        console.log('computer wins');
     }
-    else {
-        console.log('Tie game')
+    else{
+        console.log('Tie');
     }
 }
+
 
 const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
-rock.addEventListener('click', ()=>{
-    console.log('r');
-});
-paper.addEventListener('click', ()=>{
-    console.log('p');
-});
-scissors.addEventListener('click', ()=>{
-    console.log('s');
-});
+rock.addEventListener('click', initiate('rock'));
+paper.addEventListener('click', initiate('paper'));
+scissors.addEventListener('click', initiate('scissors'));
+
+function initiate(playerChoice){
+    if(gameEnd()){
+        showEndGame();
+        return ;
+    }
+    const computerChoice = getComputerChoice();
+    playRound(playerChoice,computerChoice);
+}
